a8154bfb1e3ff6ef6f7e7c66a65aa700e741da9f
Rudranshu Pandey <rudranshupandey958@gmail.com>
2026-03-18 01:14:29 +0530

    feat: add Java quickstart POM and initial AWS S3 object storage operator

--- s3stream/src/main/java/com/automq/stream/s3/operator/AwsObjectStorage.java
+++ s3stream/src/main/java/com/automq/stream/s3/operator/AwsObjectStorage.java
@@ -57,6 +57,7 @@ import software.amazon.awssdk.core.async.AsyncResponseTransformer;
 import software.amazon.awssdk.core.client.config.ClientOverrideConfiguration;
 import software.amazon.awssdk.core.exception.ApiCallAttemptTimeoutException;
 import software.amazon.awssdk.core.exception.SdkClientException;
+import software.amazon.awssdk.core.retry.RetryPolicy;
 import software.amazon.awssdk.core.retry.RetryMode;
 import software.amazon.awssdk.http.HttpStatusCode;
 import software.amazon.awssdk.http.async.SdkAsyncHttpClient;
@@ -441,7 +442,7 @@ public class AwsObjectStorage extends AbstractObjectStorage {
         return ClientOverrideConfiguration.builder()
             .apiCallTimeout(Duration.ofMillis(apiCallTimeoutMs))
             .apiCallAttemptTimeout(Duration.ofMillis(apiCallAttemptTimeoutMs))
-            .retryStrategy(RetryMode.STANDARD)
+            .retryPolicy(RetryPolicy.forRetryMode(RetryMode.STANDARD))
             .build();
     }
 
